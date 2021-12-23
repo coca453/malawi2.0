cde1b5cc0f3b17797deb47963c63096dad01ef0b
DESKTOP-4N5G45S\lucas <martinellilucas8@gmail.com>
2021-12-23 16:40:01 -0300

    Swiper autoplay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { SwiperComponent } from './shared/swiper/swiper.component';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwiperComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
