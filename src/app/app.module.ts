b4ea9e188558d9c2c3d357e978d4f40cb40e429b
Coca <coca453@gmail.com>
2021-12-23 17:03:31 -0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { SwiperComponent } from "./shared/swiper/swiper.component";
+
+import { SwiperModule } from "swiper/angular";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwiperComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/swiper/swiper.component.ts
@@ -0,0 +1,22 @@
+import { Component, ViewEncapsulation, ViewChild, OnInit } from "@angular/core";
+
+// import Swiper core and required modules
+import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([Autoplay, Pagination, Navigation]);
+
+
+@Component({
+  selector: 'app-swiper',
+  templateUrl: './swiper.component.html',
+  styleUrls: ['./swiper.component.scss']
+})
+export class SwiperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
