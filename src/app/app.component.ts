6226edac01cf02391034e90605b701708063cdf4
Coca <coca453@gmail.com>
2021-12-29 10:58:49 -0300

    barra superior finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  host: { "(window:resize)" : "onWindowResize($event)"}
+  host: { "(window:resize)": "onWindowResize($event)" }
 })
 export class AppComponent implements OnInit {
   title = 'malawiEgresados';
+
+  //mobile
   isMobile: boolean = false;
-  width:number = window.innerWidth;
-  height:number = window.innerHeight;
-  mobileWidth:number  = 760;
-  dato : string = "";
+  width: number = window.innerWidth;
+  height: number = window.innerHeight;
+  mobileWidth: number = 760;
+  dato: string = "";
 
-  ngOnInit() : void {
-      this.isMobile = this.width < this.mobileWidth;
+
+
+  //Scroll
+  scrollnumber: number = 0;
+  isScrolled = false;
+  scroll : boolean = false;
+  @HostListener("window:scroll")
+  scrollEvent() {
+    window.pageYOffset >= 80 ? (this.isScrolled = true) : (this.isScrolled = false);
+    this.scrollnumber = window.pageYOffset;
+    if (this.scrollnumber > 5) {
+      this.scroll = true;
+      console.log(this.scroll); 
+    } else {
+      this.scroll = false;
+    }
+    console.log(window.pageYOffset);
   }
 
 
-    onWindowResize(event: { target: { innerWidth: number; innerHeight: number; }; }) {
-      this.width = event.target.innerWidth;
-      this.height = event.target.innerHeight;
-      this.isMobile = this.width < this.mobileWidth;
-  }
 
-  constructor(){
+  ngOnInit(): void {
+    this.isMobile = this.width < this.mobileWidth;
+    this.scrollEvent();
 
   }
+
+
+
+  onWindowResize(event: { target: { innerWidth: number; innerHeight: number; }; }) {
+    this.width = event.target.innerWidth;
+    this.height = event.target.innerHeight;
+    this.isMobile = this.width < this.mobileWidth;
+  }
+
+  constructor() {
+
+  }
+
 
 }
--- src/app/shared/header/navbar/navbar.component.ts
+++ src/app/shared/header/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  @Input('scrollEvent') scroll : number = 0;
+  scrolled : boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  ngOnChanges(): void {
+    console.log('scroll: ' + this.scroll);  
+    if (this.scroll > 5){
+      this.scrolled = true;
+    }else{
+      this.scrolled = false;
+    }
+  }
+
+
 
 }
